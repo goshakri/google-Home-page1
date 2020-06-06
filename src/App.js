54ffbf0720f47ac25a9dc35537b36428f703058e
goshakri <goshakri223@gmail.ocm>
2020-06-06 14:00:30 +0300

    bad-test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="Page">
+      <div className="Page-header">
+        <div className="SiteNavigation">
+          <div className="SiteNavigation-item"></div>
+          <div className="SiteNavigation-item">
+            <a href="#">About</a>
+          </div>
+          <div className="SiteNavigation-item">
+            <a href="#">Store</a>
+          </div>
+        </div>
+        <div className="SiteNavigation">
+          <div className="SiteNavigation-item">
+            <a href="#">Gmail</a>
+          </div>
+          <div className="SiteNavigation-item">
+            <a href="#">Images</a>
+          </div>
+          <div className="SiteNavigation-item">
+            <a href="#"></a>
+            <img src="assets/images/bar.png" height="30px" width="30px" />
+          </div>
+          <div className="SiteNavigation-item SiteNavigation-item--button">
+            <a href="#">Sign In</a>
+          </div>
+        </div>
+      </div>
+      <div className="Page-content">
+        <a href="#" className="logo">
+          <img src="assets/images/logog.png" />
         </a>
-      </header>
+
+        <input type="text" className="search" />
+
+        <div className="flexbox">
+          <button className="google__search__btn">Google Seacrh</button>
+
+          <button className="random__search__btn">I'm Feelling Lucky</button>
+        </div>
+
+        <p className="google__text">
+          Google offered in:
+          <a href="#" style="color: blue;">
+            Russion
+          </a>
+        </p>
+      </div>
+      <div className="Page-footer">
+        <a href="#" className="advertising">
+          Adveristing
+        </a>
+        <a href="#" className="business">
+          Business
+        </a>
+        <a href="#" className="allgoogle">
+          How Search works
+        </a>
+        <a href="#" className="privacy">
+          Privacy
+        </a>
+        <a href="#" className="nouname">
+          Tems
+        </a>
+        <a href="#" className="settings">
+          Settings
+        </a>
+      </div>
     </div>
   );
 }
