d2032e77501f512a1f6be5376d1ecf4fb0d5b217
goshakri <goshakri223@gmail.ocm>
2020-06-10 21:04:19 +0300

    Fix formatting

--- src/App.js
+++ src/App.js
@@ -4,56 +4,58 @@ function App() {
     <div className="Page">
       <header className="Page-header">
         <nav>
-            <ul className="SiteNavigation">
-              <li className="SiteNavigation-item"></li>
-              <li className="SiteNavigation-item">
-                <a href="#">About</a>
-              </li>
-              <li className="SiteNavigation-item">
-                <a href="#">Store</a>
-              </li>
-            </ul>
-              <ul className="SiteNavigation">
-                <li className="SiteNavigation-item">
-                  <a href="#">Gmail</a>
-                </li>
-                <li className="SiteNavigation-item">
-                  <a href="#">Images</a>
-                </li>
-                <li className="SiteNavigation-item">
-                  <a href="#"></a>
-                  <img src="assets/images/bar.png" height="30px" width="30px" />
-                </li>
-                <li className="SiteNavigation-item SiteNavigation-item--button">
-                  <a href="#">Sign In</a>
-                </li>
-            </ul>
+          <ul className="SiteNavigation">
+            <li className="SiteNavigation-item"></li>
+            <li className="SiteNavigation-item">
+              <a href="#">About</a>
+            </li>
+            <li className="SiteNavigation-item">
+              <a href="#">Store</a>
+            </li>
+          </ul>
+
+          <ul className="SiteNavigation">
+            <li className="SiteNavigation-item">
+              <a href="#">Gmail</a>
+            </li>
+            <li className="SiteNavigation-item">
+              <a href="#">Images</a>
+            </li>
+            <li className="SiteNavigation-item">
+              <a href="#"></a>
+              <img src="assets/images/bar.png" height="30px" width="30px" />
+            </li>
+            <li className="SiteNavigation-item SiteNavigation-item--button">
+              <a href="#">Sign In</a>
+            </li>
+          </ul>
         </nav>
       </header>
-          <div className="Page-content">
-            <a href="#" className="logo">
-              <img src="assets/images/logog.png" />
-            </a>
 
-            <input type="text" className="search" />
+      <div className="Page-content">
+        <a href="#" className="logo">
+          <img src="assets/images/logog.png" />
+        </a>
 
-            <div className="flexbox">
-              <button className="google__search__btn">Google Seacrh</button>
+        <input type="text" className="search" />
 
-              <button className="random__search__btn">I'm Feelling Lucky</button>
-            </div>
+        <div className="flexbox">
+          <button className="google__search__btn">Google Seacrh</button>
 
-            <p className="google__text">Google offered in:</p>
-          </div>
-          <div className="Page-footer">
-            <a href="#">Adveristing</a>
-            <a href="#">Business</a>
-            <a href="#">How Search works</a>
-            <a href="#">Privacy</a>
-            <a href="#">Tems</a>
-            <a href="#">Settings</a>
-          </div>
+          <button className="random__search__btn">I'm Feelling Lucky</button>
         </div>
+
+        <p className="google__text">Google offered in:</p>
+      </div>
+      <div className="Page-footer">
+        <a href="#">Adveristing</a>
+        <a href="#">Business</a>
+        <a href="#">How Search works</a>
+        <a href="#">Privacy</a>
+        <a href="#">Tems</a>
+        <a href="#">Settings</a>
+      </div>
+    </div>
   );
 }
 export default App;
