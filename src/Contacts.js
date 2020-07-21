5bb8fb7854d562f7fb7152889a177df2d520f209
goshakri <goshakri223@gmail.ocm>
2020-07-21 21:13:58 +0300

    Contacts page

---
+++ src/Contacts.js
@@ -0,0 +1,79 @@
+import React from "react";
+import google from "./images/google.jpg";
+
+function Contacts() {
+  return (
+    <div className="Page">
+      <header>
+        <div className="Section Section--zircon">
+          <div className="Section-content">
+            <nav className="SiteNavigation">
+              <ul className="SiteNavigation-column">
+                <li className="SiteNavigation-item">
+                  <a className="SiteNavigation-link" href="#">
+                    Search
+                  </a>
+                </li>
+                <li className="SiteNavigation-item">
+                  <a className="SiteNavigation-link" href="#">
+                    Contacts
+                  </a>
+                </li>
+              </ul>
+
+              <ul className="SiteNavigation-column">
+                <li className="SiteNavigation-item">
+                  <a className="SiteNavigation-link" href="#">
+                    Examples
+                  </a>
+                </li>
+                <li className="SiteNavigation-item">
+                  <a className="Button" href="#">
+                    Sign In
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </header>
+
+      <main className="Page-content">
+        <div className="Section">
+          <div className="Section-content">
+            <div className="SearchPage">
+              <div className="SearchPage-picture">
+                <img className="SearchPage-pictureItem" src={google} alt="" />
+              </div>
+
+              <div className="SearchPage-input">
+                <input type="text" className="Field"/>
+              </div>
+
+              <div className="SearchPage-actions">
+                <button className="Button Button--spaced">Google Seacrh</button>
+                <button className="Button">I'm Feelling Lucky</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+
+      <footer>
+        <div className="Section Section--zircon">
+          <div className="Section-content">
+            <div className="PageFooter">
+              <p>
+                Created by{" "}
+                <a className="Link" href="https://github.com">
+                  goshakri
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
+export default Contacts;
--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import App from "./App";
+import Contacts from "./pages/Contacts";
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/">
         <App />
+      </Route>
+      <Route exact path="/contacts">
+        <Contacts />
       </Route>
     </Switch>
   );
