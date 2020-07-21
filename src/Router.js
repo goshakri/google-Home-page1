4bd4410ab9d12fc10ae269179ddbcbf382e6a0e6
goshakri <goshakri223@gmail.ocm>
2020-07-21 21:28:21 +0300

    Fix the path to the file

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import App from "./App";
-import Contacts from "./pages/Contacts";
+import Contacts from "./Contacts";
 
 export default function Routes() {
   return (
