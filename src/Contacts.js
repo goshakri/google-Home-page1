777602be67eb7203a2bc15cb8a5400841f18be88
goshakri <goshakri223@gmail.ocm>
2020-07-22 23:36:32 +0300

    Update position and corect code

--- src/Contacts.js
+++ src/Contacts.js
@@ -1,77 +1,88 @@
-import React from "react"; import google from "./images/google.jpg"; function Contacts() { return (
-<div className="Page">
-  <header>
-    <div className="Section Section--zircon">
-      <div className="Section-content">
-        <nav className="SiteNavigation">
-          <ul className="SiteNavigation-column">
-            <li className="SiteNavigation-item">
-              <a className="SiteNavigation-link" href="#">
-                Search
-              </a>
-            </li>
-            <li className="SiteNavigation-item">
-              <a className="SiteNavigation-link" href="#">
-                Contacts
-              </a>
-            </li>
-          </ul>
+import React from "react";
+import google from "./images/google.jpg";
+function Contacts() {
+  return (
+    <div className="Page">
+      <header>
+        <div className="Section Section--zircon">
+          <div className="Section-content">
+            <nav className="SiteNavigation">
+              <ul className="SiteNavigation-column">
+                <li className="SiteNavigation-item">
+                  <a className="SiteNavigation-link" href="#">
+                    Search
+                  </a>
+                </li>
+                <li className="SiteNavigation-item">
+                  <a className="SiteNavigation-link" href="#">
+                    Contacts
+                  </a>
+                </li>
+              </ul>
 
-          <ul className="SiteNavigation-column">
-            <li className="SiteNavigation-item">
-              <a className="SiteNavigation-link" href="#">
-                Examples
-              </a>
-            </li>
-            <li className="SiteNavigation-item">
-              <a className="Button" href="#">
-                Sign In
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </div>
-  </header>
-
-  <main className="Page-content">
-    <div className="Section">
-      <div className="Section-content">
-
-        <div className="Text">
-          <h1>Please fill the from to make it filled.</h1>
+              <ul className="SiteNavigation-column">
+                <li className="SiteNavigation-item">
+                  <a className="SiteNavigation-link" href="#">
+                    Examples
+                  </a>
+                </li>
+                <li className="SiteNavigation-item">
+                  <a className="Button" href="#">
+                    Sign In
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
+      </header>
 
-        <div className="SearchPage">
-          <form className="SearchPage-region">
-            <label for="name">Name:</label><br />
-            <input className="" type="text" name="username" /><br />
-            <label for="gmail">Email:</label><br />
-            <input className="" type="gmail" name="email" /><br />
-            <label for="city">City:</label><br />
-            <input className="" type="city" name="city" /><br />
-            <label for="massage">Massage:</label><br />
-            <input className="" type="massage" name="massage" /><br />
-          </form>
+      <main className="Page-content">
+        <div className="Section">
+          <div className="Section-content">
+            <div className="ContactsPage">
+              <div className="Text">
+                <p>Please fill the from to make it filled.</p>
+              </div>
+
+              <form className="ContactsPage-block">
+                <label for="name">Name:</label>
+                <br />
+                <input className="" type="text" name="username" />
+                <br />
+                <label for="gmail">Email:</label>
+                <br />
+                <input className="" type="gmail" name="email" />
+                <br />
+                <label for="city">City:</label>
+                <br />
+                <input className="" type="city" name="city" />
+                <br />
+                <label for="massage">Massage:</label>
+                <br />
+                <input className="" type="massage" name="massage" />
+                <br />
+              </form>
+            </div>
+          </div>
         </div>
+      </main>
 
-      </div>
-    </div>
-  </main>
-
-  <footer>
-    <div className="Section Section--zircon">
-      <div className="Section-content">
-        <div className="PageFooter">
-          <p>
-            Created by{" "}
-            <a className="Link" href="https://github.com">
-              goshakri
-            </a>
-          </p>
+      <footer>
+        <div className="Section Section--zircon">
+          <div className="Section-content">
+            <div className="PageFooter">
+              <p>
+                Created by{" "}
+                <a className="Link" href="https://github.com">
+                  goshakri
+                </a>
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
+      </footer>
     </div>
-  </footer>
-</div>
-); } export default Contacts;
+  );
+}
+export default Contacts;
