543d107fc94650ac0926bdaf6b05dc1a69110d90
goshakri <goshakri223@gmail.ocm>
2020-07-31 00:40:18 +0300

    Last change

--- src/Contacts.js
+++ src/Contacts.js
@@ -42,61 +42,65 @@ function Contacts() {
           <div className="Section-content">
             <div className="ContactsPage">
 
-              <div className="ContactsPage-text">
-                <p>Please fill the from to make it filled.</p>
+              <div className="ContactsPage-textblock">
+                <div className="ContactsPage-text">
+                  <p>Please fill the from to make it filled.</p>
+                </div>
               </div>
 
-              <form className="Form">
-                <div className="ContactsPage-block">
-                  <div className="ContactsPage-one">
-                    <label className="ContactsPage-pizza" for="name">
-                      Name:
-                    </label>
-                    <br />
-                    <input className="ContactsPage-test" type="text" id="name" name="username" />
-                    <br />
-                    <label className="ContactsPage-pizza" for="gmail">
-                      Email:
-                    </label>
-                    <br />
-                    <input className="ContactsPage-test" type="gmail" id="gmail" name="email" />
-                    <br />
-                    <label className="ContactsPage-pizza" for="city">
-                      City:
-                    </label>
-                    <br />
-                    <input className="ContactsPage-test" type="city" id="city" name="city" />
-                    <br />
+              <div className="ContactsPage-formblock">
+                <form className="Form">
+                  <div className="ContactsPage-block">
+                    <div className="ContactsPage-one">
+                      <label className="ContactsPage-pizza" for="name">
+                        Name:
+                      </label>
+                      <br />
+                      <input className="ContactsPage-test" type="text" id="name" name="username" />
+                      <br />
+                      <label className="ContactsPage-pizza" for="gmail">
+                        Email:
+                      </label>
+                      <br />
+                      <input className="ContactsPage-test" type="gmail" id="gmail" name="email" />
+                      <br />
+                      <label className="ContactsPage-pizza" for="city">
+                        City:
+                      </label>
+                      <br />
+                      <select className="ContactsPage-test" name="city" id="city">
+                      </select>
+                      <br />
+                    </div>
+
+                    <div className="ContactsPage-two">
+                      <label className="ContactsPage-pizza" for="sex">
+                        Sex:
+                      </label>
+                      <br />
+                      <input className="ContactsPage ContactsPage--" type="radio" id="sex" name="sex" />
+                      <input className="ContactsPage ContactsPage--" type="radio" id="sex" name="sex" />
+                      <br />
+
+                      <label className="" for="massage">
+                        Massage:
+                      </label>
+                      <br />
+                      <textarea className="" id="massage" name="massage"></textarea>
+                      <br />
+                      <label for="agree"> Agree</label>
+                      <br />
+                      <input type="checkbox" id="agree" name="agree" value="agree" />
+                      <br />
+
+                      <div className="ContactsPage-btn">
+                        <button className="Button Button-send">Send</button>
+                      </div>
+                    </div>
+
                   </div>
-
-                  <div className="ContactsPage-two">
-                    <label className="ContactsPage-pizza" for="sex">
-                      Sex:
-                    </label>
-                    <br />
-                    <input className="ContactsPage ContactsPage--" type="radio" id="sex" name="sex" />
-                    <input className="ContactsPage ContactsPage--" type="radio" id="sex" name="sex" />
-                    <br />
-
-                    <label className="" for="massage">
-                      Massage:
-                    </label>
-                    <br />
-                    <textarea className="" id="massage" name="massage"></textarea>
-                    <br />
-                    <label for="agree"> Agree
-                    </label>
-                    <br />
-                    <input type="checkbox" id="agree" name="agree" value="agree" />
-                    <br />
-                  </div>
-
-                  <div className="ContactsPage-btn">
-                    <button className="Button Button-send">Send</button>
-                  </div>
-
-                </div>
-              </form>
+                </form>
+              </div>
             </div>
           </div>
         </div>
