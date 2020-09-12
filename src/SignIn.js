a4fe98facf610284ea2ad9f0417d56d2f9ad258e
goshakri <goshakri223@gmail.ocm>
2020-09-12 15:42:40 +0300

    Starting created Sign In

--- src/SignIn.js
+++ src/SignIn.js
@@ -8,7 +8,30 @@ function SignIn() {
       <Header />
 
       <main className="Page-content">
+        <div className="Section">
+          <div className="Section-content">
+            <div className="Row">
+              <div className="Row-eddition u-pt-50 u-pb-50 u-pr-10">
 
+                <h1>Welcome</h1>
+
+                <form>
+                  <div className="">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" name="email" />
+                  </div>
+
+                  <div className="">
+                    <label htmlFor="email">Password</label>
+                    <input type="password" id="password" name="password" />
+                  </div>
+
+                </form>
+
+              </div>
+            </div>
+          </div>
+        </div>
       </main>
 
       <Footer />
