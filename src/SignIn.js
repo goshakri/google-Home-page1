73821a46bb80bb16aa0c58c57c75cfe968e8fc9c
goshakri <goshakri223@gmail.ocm>
2020-12-08 00:02:25 +0200

    Created 1/3 register

--- src/SignIn.js
+++ src/SignIn.js
@@ -10,7 +10,24 @@ function SignIn() {
       <main className="Page-content">
         <div className="Section">
           <div className="Section-content">
+            <div className="WrapLogin u-pt-90">
+              <form className="Form">
+                <span className="Text Text--login u-pb-40">Login</span>
 
+                <div className="u-pt-5">
+                  <a href="#">Login with Facebook</a>
+                </div>
+
+                <div className="u-pt-5">
+                  <a href="#">Login with Twitter</a>
+                </div>
+
+                <div className="Input">
+                  <input className="Field Field--register" type="text" name="email" placeholder="Email" ></input>
+                </div>
+
+              </form>
+            </div>
           </div>
         </div>
       </main>
