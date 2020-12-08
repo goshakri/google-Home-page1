0ea07889051810e658694083105ae8e7aa775ec9
goshakri <goshakri223@gmail.ocm>
2020-12-09 00:04:34 +0200

    Created new link facebook and twitter

--- src/SignIn.js
+++ src/SignIn.js
@@ -15,11 +15,11 @@ function SignIn() {
                 <span className="Text Text--login u-pb-40">Login</span>
 
                 <div className="u-pt-5">
-                  <a href="#">Login with Facebook</a>
+                  <a className="Button Button--linkbgone" href="#">Login with Facebook</a>
                 </div>
 
                 <div className="u-pt-5">
-                  <a href="#">Login with Twitter</a>
+                  <a className="Button Button--linkbgtwo" href="#">Login with Twitter</a>
                 </div>
 
                 <div className="Input">
